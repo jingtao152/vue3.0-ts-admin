71e3c4ff9f6526050de8348f92061ab717ed5588
jingtao <919310421@qq.com>
2021-01-14 13:39:20 +0800

    新增验证码功能，第三方登录，历史记录页

---
+++ src/util/util.ts
@@ -0,0 +1,20 @@
+/**
+ * 打开小窗口
+ */
+export const openWindow = (url, title, w, h) => {
+  // Fixes dual-screen position                            Most browsers       Firefox
+  const dualScreenLeft = window.screenLeft !== undefined ? window.screenLeft : screenX
+  const dualScreenTop = window.screenTop !== undefined ? window.screenTop : screenY
+
+  const width = window.innerWidth ? window.innerWidth : document.documentElement.clientWidth ? document.documentElement.clientWidth : screen.width
+  const height = window.innerHeight ? window.innerHeight : document.documentElement.clientHeight ? document.documentElement.clientHeight : screen.height
+
+  const left = ((width / 2) - (w / 2)) + dualScreenLeft
+  const top = ((height / 2) - (h / 2)) + dualScreenTop
+  const newWindow = window.open(url, title, 'toolbar=no, location=no, directories=no, status=no, menubar=no, scrollbars=no, resizable=yes, copyhistory=no, width=' + w + ', height=' + h + ', top=' + top + ', left=' + left)
+
+  // Puts focus on the newWindow
+    if (newWindow) {
+      newWindow.focus()
+    }
+}
